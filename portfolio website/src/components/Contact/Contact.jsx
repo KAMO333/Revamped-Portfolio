8e7f2fdd55c3efc2ac451fc49947f241ff4119ca
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-12-30 19:24:03 +0200

    contact section

---
+++ portfolio website/src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:kamosworkemail@gmail.com">kamosworkemail@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/kamogelommopane/">
+            linkedin.com/kamogelommopane
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/KAMO333">github.com/KAMO333</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
